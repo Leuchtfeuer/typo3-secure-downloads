1f7b75b5dd24240b52ba9772fd36e22bddb52da5
Helmut Hummel <h.hummel@bitmotion.de>
2010-04-30 11:32:01 +0000

    * emconf after upload to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "naw_securedl"
 #
-# Auto generated 30-01-2009 16:29
+# Auto generated 30-04-2010 13:31
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"120e";s:8:"TODO.txt";s:4:"014d";s:24:"class.tx_nawsecuredl.php";s:4:"7b2b";s:31:"class.tx_nawsecuredl_output.php";s:4:"9c8a";s:29:"class.ux_SC_tslib_showpic.php";s:4:"6519";s:28:"class.ux_class.file_list.inc";s:4:"7ebe";s:21:"ext_conf_template.txt";s:4:"98a7";s:12:"ext_icon.gif";s:4:"2dbb";s:17:"ext_localconf.php";s:4:"37d6";s:14:"ext_tables.sql";s:4:"9554";s:14:"res/_.htaccess";s:4:"f453";s:20:"res/_.htaccess_allow";s:4:"01b3";s:19:"res/_.htaccess_deny";s:4:"f453";s:14:"doc/manual.sxw";s:4:"7bea";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"fd59";s:8:"TODO.txt";s:4:"014d";s:24:"class.tx_nawsecuredl.php";s:4:"90ef";s:28:"class.tx_nawsecuredl_log.php";s:4:"6f17";s:31:"class.tx_nawsecuredl_output.php";s:4:"85c9";s:29:"class.ux_SC_tslib_showpic.php";s:4:"6519";s:21:"class.ux_fileList.inc";s:4:"7ebe";s:21:"ext_conf_template.txt";s:4:"77e2";s:12:"ext_icon.gif";s:4:"2dbb";s:17:"ext_localconf.php";s:4:"7271";s:14:"ext_tables.php";s:4:"9fc5";s:14:"ext_tables.sql";s:4:"ef63";s:14:"res/_.htaccess";s:4:"e99d";s:20:"res/_.htaccess_allow";s:4:"5057";s:19:"res/_.htaccess_deny";s:4:"e99d";s:37:"modLog/class.tx_nawsecuredl_table.php";s:4:"dbd7";s:15:"modLog/conf.php";s:4:"9c2f";s:16:"modLog/index.php";s:4:"aa9d";s:20:"modLog/locallang.xml";s:4:"85bc";s:24:"modLog/locallang_mod.xml";s:4:"3833";s:21:"modLog/moduleicon.gif";s:4:"691d";s:14:"doc/manual.sxw";s:4:"40d1";}',
 	'suggests' => array(
 	),
 );
