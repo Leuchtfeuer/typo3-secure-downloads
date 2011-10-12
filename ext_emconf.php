d417aa8c7bd1eeab683393fea15e308e8b5e3e5e
heise <heise@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-12 09:47:06 +0000

    * Version 1.5.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/naw_securedl/trunk@52925 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "naw_securedl".
 #
-# Auto generated 14-03-2011 19:27
+# Auto generated 12-10-2011 11:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"81f2";s:8:"TODO.txt";s:4:"014d";s:24:"class.tx_nawsecuredl.php";s:4:"2822";s:28:"class.tx_nawsecuredl_log.php";s:4:"3977";s:31:"class.tx_nawsecuredl_output.php";s:4:"0eda";s:29:"class.ux_SC_tslib_showpic.php";s:4:"6519";s:21:"class.ux_fileList.inc";s:4:"eb23";s:21:"ext_conf_template.txt";s:4:"c4e1";s:12:"ext_icon.gif";s:4:"2dbb";s:17:"ext_localconf.php";s:4:"7271";s:14:"ext_tables.php";s:4:"9fc5";s:14:"ext_tables.sql";s:4:"ec93";s:14:"doc/manual.sxw";s:4:"6b88";s:37:"modLog/class.tx_nawsecuredl_table.php";s:4:"361d";s:15:"modLog/conf.php";s:4:"9c2f";s:16:"modLog/index.php";s:4:"92fc";s:20:"modLog/locallang.xml";s:4:"85bc";s:24:"modLog/locallang_mod.xml";s:4:"3833";s:21:"modLog/moduleicon.gif";s:4:"691d";s:14:"res/_.htaccess";s:4:"e99d";s:20:"res/_.htaccess_allow";s:4:"5057";s:19:"res/_.htaccess_deny";s:4:"e99d";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"f217";s:8:"TODO.txt";s:4:"014d";s:24:"class.tx_nawsecuredl.php";s:4:"1b98";s:28:"class.tx_nawsecuredl_log.php";s:4:"3977";s:31:"class.tx_nawsecuredl_output.php";s:4:"e474";s:29:"class.ux_SC_tslib_showpic.php";s:4:"6519";s:21:"class.ux_fileList.inc";s:4:"1553";s:21:"ext_conf_template.txt";s:4:"c4e1";s:12:"ext_icon.gif";s:4:"2dbb";s:17:"ext_localconf.php";s:4:"7271";s:14:"ext_tables.php";s:4:"9fc5";s:14:"ext_tables.sql";s:4:"ec93";s:14:"doc/manual.sxw";s:4:"6b88";s:37:"modLog/class.tx_nawsecuredl_table.php";s:4:"361d";s:15:"modLog/conf.php";s:4:"9c2f";s:16:"modLog/index.php";s:4:"92fc";s:20:"modLog/locallang.xml";s:4:"85bc";s:24:"modLog/locallang_mod.xml";s:4:"3833";s:21:"modLog/moduleicon.gif";s:4:"691d";s:14:"res/_.htaccess";s:4:"e99d";s:20:"res/_.htaccess_allow";s:4:"5057";s:19:"res/_.htaccess_deny";s:4:"e99d";}',
 	'suggests' => array(
 	),
 );
