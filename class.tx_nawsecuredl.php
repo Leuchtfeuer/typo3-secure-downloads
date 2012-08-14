1e695152f801efe135f840ce7988bb11ccbda0fd
helhum <helhum@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-08-14 13:52:55 +0000

    [TASK] Refactor secure link generation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/naw_securedl/trunk@65400 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_nawsecuredl.php
+++ class.tx_nawsecuredl.php
@@ -1,27 +1,27 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2005-2011 Dietrich Heise (typo3-ext(at)naw.info)
-*  (c) 2009-2011 Helmut Hummel (typo3-ext(at)naw.info)
-*  All rights reserved
-*
-*  This script is part of the Typo3 project. The Typo3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2005-2011 Dietrich Heise (typo3-ext(at)naw.info)
+ *  (c) 2009-2011 Helmut Hummel (typo3-ext(at)naw.info)
+ *  All rights reserved
+ *
+ *  This script is part of the Typo3 project. The Typo3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  * @author	Dietrich Heise <typo3-ext(at)naw.info>
@@ -34,14 +34,7 @@ class tx_nawsecuredl {
 	 *
 	 * @var array
 	 */
-	protected $extConf;
-
-	/**
-	 * Frontend user id
-	 *
-	 * @var integer
-	 */
-	protected $feuser;
+	protected $extensionConfiguration;
 
 	/**
 	 * The TYPO3 frontend object
@@ -56,7 +49,7 @@ class tx_nawsecuredl {
 	 */
 	public function __construct() {
 		$this->objFrontend = $GLOBALS['TSFE'];
-		$this->extConf = $this->getExtensionConfiguration();
+		$this->extensionConfiguration = $this->getExtensionConfiguration();
 	}
 
 	/**
@@ -77,9 +70,9 @@ class tx_nawsecuredl {
 	public function parseFE(&$parameters, $objFrontend) {
 		$this->objFrontend = $objFrontend;
 
-			// Parsing the content if not explicitly disabled
+		// Parsing the content if not explicitly disabled
 		if (!isset($this->objFrontend->config['config']['tx_nawsecuredl_enable'])
-				|| $this->objFrontend->config['config']['tx_nawsecuredl_enable'] !== '0') {
+			|| $this->objFrontend->config['config']['tx_nawsecuredl_enable'] !== '0') {
 			$this->objFrontend->content = $this->parseContent($this->objFrontend->content);
 		}
 	}
@@ -91,64 +84,76 @@ class tx_nawsecuredl {
 	 * @return string
 	 */
 	public function parseContent($strContent) {
-		$this->extConf = $this->getExtensionConfiguration();
+		$this->extensionConfiguration = $this->getExtensionConfiguration();
 		$rest = $strContent;
 		$result = '';
 		while (preg_match('/(?i)(<source|<a|<img)+?.[^>]*(href|src)=(\"??)([^\" >]*?)\\3[^>]*>/siU', $strContent, $match)) {  // suchendes secured Verzeichnis
 			$cont = explode($match[0], $strContent, 2);
 			$vor = $cont[0];
 			$tag = $match[0];
-			if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
 				debug('tag:' . $tag);
 			}
 
 			$rest = $cont[1];
 
-			if ($this->extConf['debug'] == '1' || $this->extConf['debug'] == '3') {
+			if ($this->extensionConfiguration['debug'] == '1' || $this->extensionConfiguration['debug'] == '3') {
 				debug(array('html-tag:'=>$tag));
 			}
 
-				// investigate the HTML-Tag...
-			if (preg_match('/"(?:'.$this->modifiyregex($this->extConf['domain']).')?(\/?(?:'.$this->modifiyregex($this->extConf['securedDirs']).')+?.*?(?:'.$this->modifyfiletypes($this->extConf['filetype']).'))"/i', $tag, $match1)) {
+			$tag = $this->parseHtmlTag($tag, $this->extensionConfiguration['securedDirs']);
 
-				if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
-					debug('/"(?:'.$this->modifiyregex($this->extConf['domain']).')?(\/?(?:'.$this->modifiyregex($this->extConf['securedDirs']).')+?.*?(?:'.$this->modifyfiletypes($this->extConf['filetype']).'))"/i');
-				}
-				if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
-					debug($match1);
-				}
-
-				$replace = htmlspecialchars($this->makeSecure($match1[1]));
-				$tagexp = explode($match1[1], $tag , 2 );
-
-				if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
-					debug($tagexp[0]);
-				}
-				if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
-					debug($replace);
-				}
-				if ($this->extConf['debug'] == '2' || $this->extConf['debug'] == '3') {
-					debug($tagexp[1]);
-				}
-
-				$tag = $tagexp[0] . $replace;
-				$tmp = $tagexp[1];
-
-					// search in the rest on the tag (e.g. for vHWin=window.open...)
-				if (preg_match('/\'(?:'.$this->modifiyregex($this->extConf['domain']).')?.*?(\/?(?:'.$this->modifiyregex($this->extConf['securedDirs']).')+?.*?(?:'.$this->modifyfiletypes($this->extConf['filetype']).'))\'/i', $tmp, $match1)){
-					$replace = htmlspecialchars($this->makeSecure($match1[1]));
-					$tagexp = explode ($match1[1], $tmp , 2 );
-					$add = $tagexp[0].'/'.$replace.$tagexp[1];
-				} else {
-					$add = $tagexp[1];
-				}
-
-				$tag .= $add;
-			}
 			$result .= $vor . $tag;
 			$strContent = $rest;
 		}
 		return $result . $rest;
+	}
+
+	/**
+	 * Investigate the HTML-Tag...
+	 *
+	 * @param $tag
+	 * @param $toSecureDirectoryExpression
+	 * @return string
+	 */
+	protected function parseHtmlTag($tag, $toSecureDirectoryExpression) {
+		if (preg_match('/"(?:' . $this->softQuoteExpression($this->extensionConfiguration['domain']) . ')?(\/?(?:' . $this->softQuoteExpression($toSecureDirectoryExpression) . ')+?.*?(?:' . $this->getFileTypeExpression($this->extensionConfiguration['filetype']) . '))"/i', $tag, $matchedUrls)) {
+
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
+				debug('/"(?:' . $this->softQuoteExpression($this->extensionConfiguration['domain']) . ')?(\/?(?:' . $this->softQuoteExpression($toSecureDirectoryExpression) . ')+?.*?(?:' . $this->getFileTypeExpression($this->extensionConfiguration['filetype']) . '))"/i');
+			}
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
+				debug($matchedUrls);
+			}
+
+			$replace = htmlspecialchars($this->makeSecure($matchedUrls[1]));
+			$tagexp = explode($matchedUrls[1], $tag, 2);
+
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
+				debug($tagexp[0]);
+			}
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
+				debug($replace);
+			}
+			if ($this->extensionConfiguration['debug'] == '2' || $this->extensionConfiguration['debug'] == '3') {
+				debug($tagexp[1]);
+			}
+
+			$tag = $tagexp[0] . $replace;
+			$tmp = $tagexp[1];
+
+			// search in the rest on the tag (e.g. for vHWin=window.open...)
+			if (preg_match('/\'(?:' . $this->softQuoteExpression($this->extensionConfiguration['domain']) . ')?.*?(\/?(?:' . $this->softQuoteExpression($toSecureDirectoryExpression) . ')+?.*?(?:' . $this->getFileTypeExpression($this->extensionConfiguration['filetype']) . '))\'/i', $tmp, $matchedUrls)) {
+				$replace = htmlspecialchars($this->makeSecure($matchedUrls[1]));
+				$tagexp = explode($matchedUrls[1], $tmp, 2);
+				$add = $tagexp[0] . '/' . $replace . $tagexp[1];
+			} else {
+				$add = $tagexp[1];
+			}
+
+			$tag .= $add;
+		}
+		return $tag;
 	}
 
 	/**
@@ -159,34 +164,35 @@ class tx_nawsecuredl {
 	 */
 	public function makeSecure($originalUrl) {
 		if ($this->objFrontend->fe_user->user['uid']){
-			$this->feuser = $this->objFrontend->fe_user->user['uid'];
+			$frontendUserId = $this->objFrontend->fe_user->user['uid'];
+			$frontendUserGroupIds = t3lib_div::trimExplode(',', $this->objFrontend->fe_user->user['usergroup'], TRUE);
 		} else {
-			$this->feuser = 0;
+			$frontendUserId = 0;
+			$frontendUserGroupIds = array();
 		}
 
-		$cachetimeadd = $this->extConf['cachetimeadd'];
+		$cacheTimeToAdd = $this->extensionConfiguration['cachetimeadd'];
 
 		if ($this->objFrontend->page['cache_timeout'] == 0){
-			$timeout = 86400 + time() + $cachetimeadd;
+			$timeout = 86400 + $GLOBALS['EXEC_TIME'] + $cacheTimeToAdd;
 		} else {
-			$timeout =  $this->objFrontend->page['cache_timeout'] + time() + $cachetimeadd;
+			$timeout =  $this->objFrontend->page['cache_timeout'] + $GLOBALS['EXEC_TIME'] + $cacheTimeToAdd;
 		}
 
-			// $element contains the URL which is already url encoded by TYPO3.
-			// Since we check the hash in the output script using the decoded filename we must decode it here also!
-		$data = $this->feuser . rawurldecode($originalUrl) . $timeout;
-		$hash = t3lib_div::hmac($data);
+		// $element contains the URL which is already url encoded by TYPO3.
+		// Since we check the hash in the output script using the decoded filename we must decode it here also!
+		$hash = $this->getHash($frontendUserId . implode(',', $frontendUserGroupIds) . rawurldecode($originalUrl) . $timeout);
 
-			// Parsing the link format, and return this instead (an flexible link format is useful for mod_rewrite tricks ;)
-		if (!isset($this->extConf['linkFormat'])) {
-			$this->extConf['linkFormat'] = 'index.php?eID=tx_nawsecuredl&u=###FEUSER###&file=###FILE###&t=###TIMEOUT###&hash=###HASH###';
+		// Parsing the link format, and return this instead (an flexible link format is useful for mod_rewrite tricks ;)
+		if (!isset($this->extensionConfiguration['linkFormat'])) {
+			$this->extensionConfiguration['linkFormat'] = 'index.php?eID=tx_nawsecuredl&u=###FEUSER###&g=###FEGROUPS###&file=###FILE###&t=###TIMEOUT###&hash=###HASH###';
 		}
 
-		$tokens = array('###FEUSER###', '###FILE###', '###TIMEOUT###', '###HASH###');
-		$replacements = array($this->feuser, $originalUrl, $timeout, $hash);
-		$transformedUrl = str_replace($tokens, $replacements, $this->extConf['linkFormat']);
+		$tokens = array('###FEUSER###', '###FEGROUPS###', '###FILE###', '###TIMEOUT###', '###HASH###');
+		$replacements = array($frontendUserId, implode(',', $frontendUserGroupIds), $originalUrl, $timeout, $hash);
+		$transformedUrl = str_replace($tokens, $replacements, $this->extensionConfiguration['linkFormat']);
 
-			// Hook for makeSecure:
+		// Hook for makeSecure:
 		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/naw_securedl/class.tx_nawsecuredl.php']['makeSecure'])) {
 			foreach($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/naw_securedl/class.tx_nawsecuredl.php']['makeSecure'] as $_funcRef)   {
 				$transformedUrl = t3lib_div::callUserFunction($_funcRef, $transformedUrl, $this);
@@ -197,23 +203,32 @@ class tx_nawsecuredl {
 	}
 
 	/**
+	 * @param $string
+	 * @return string
+	 */
+	protected function getHash($string) {
+		return t3lib_div::hmac($string);
+	}
+
+	/**
 	 * Returns a case insensitive regular expression based on
 	 * lowercase input
 	 *
 	 * @param string $string
 	 * @return string
 	 */
-	protected function modifyfiletypes($string) {
+	protected function getFileTypeExpression($string) {
 		return '(?i)' . $string;
 	}
 
 	/**
-	 * Quotes special characters for the regular expression
+	 * Quotes special some characters for the regular expression.
+	 * Leave braces and brackets as is to have more flexibility in configuration.
 	 *
-	 * @todo: Check if this can be replaced by preg_quote()
 	 * @param string $string
+	 * @return string
 	 */
-	protected function modifiyregex($string) {
+	protected function softQuoteExpression($string) {
 		$string = str_replace('\\', '\\\\', $string);
 		$string = str_replace(' ', '\ ', $string);
 		$string = str_replace('/', '\/', $string);
