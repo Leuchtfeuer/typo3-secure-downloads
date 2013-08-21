8c6c4aaf088a7565abefed865713a00c8157d9cc
Helmut Hummel <h.hummel@bitmotion.de>
2013-08-21 16:50:33 +0200

    [TASK] Refactor URL generation to use signal
    Instead of subclassing the LocalDriver, we can
    use the signal in ResourceStorage to intercept
    public URL generation independently of the
    used storage or driver.
    
    Besides that it is cleaner and more robst.

---
+++ Classes/Resource/UrlGenerationInterceptor.php
@@ -0,0 +1,102 @@
+<?php
+namespace Bitmotion\NawSecuredl\Resource;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Helmut Hummel (helmut.hummel@typo3.org)
+ *  All rights reserved
+ *
+ *  This script is part of the Typo3 project. The Typo3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Resource\Driver\AbstractDriver;
+use TYPO3\CMS\Core\Resource\ResourceInterface;
+use TYPO3\CMS\Core\Resource\ResourceStorage;
+
+/**
+ * Class UrlGenerationInterceptor
+ * @package Bitmotion\NawSecuredl\Resource
+ */
+class UrlGenerationInterceptor {
+
+	/**
+	 * @param ResourceStorage $storage
+	 * @param AbstractDriver $driver
+	 * @param ResourceInterface $resourceObject
+	 * @param boolean $relativeToCurrentScript
+	 * @param array $urlData
+	 */
+	public function getPublicUrl(ResourceStorage $storage, AbstractDriver $driver, ResourceInterface $resourceObject, $relativeToCurrentScript, array $urlData) {
+		$publicUrl = $driver->getPublicUrl($resourceObject, FALSE);
+
+		$extensionConfiguration = $this->getSecureDlExtensionConfiguration();
+
+		if (preg_match('/(('. $this->softQuoteExpression($extensionConfiguration['securedDirs']) . ')+?\/.*?(?:(?i)' . ($extensionConfiguration['filetype']) . '))/i', $publicUrl, $matchedUrls)) {
+			if (is_array($matchedUrls)){
+				if ($matchedUrls[0] == $publicUrl){
+					$objSecureDownloads = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance
+						('Bitmotion\NawSecuredl\Service\SecureDownloadService');
+					$publicUrl = $objSecureDownloads->makeSecure($publicUrl);
+					// TODO: search better solution
+					if ( substr($publicUrl,0,1) != '/' ){
+						$publicUrl = '/' . $publicUrl;
+					}
+				}
+			}
+		}
+
+		$urlData['publicUrl'] = $publicUrl;
+
+	}
+
+	/**
+	 * Quotes special some characters for the regular expression.
+	 * Leave braces and brackets as is to have more flexibility in configuration.
+	 *
+	 * @param string $string
+	 * @return string
+	 */
+	protected function softQuoteExpression($string) {
+		return str_replace(
+			array(
+				'\\',
+				' ',
+				'/',
+				'.',
+				':'
+			),
+			array(
+				'\\\\',
+				'\ ',
+				'\/',
+				'\.',
+				'\:'
+			),
+			$string
+		);
+	}
+
+	/**
+	 * Get extension configuration
+	 *
+	 * @return array
+	 */
+	protected function getSecureDlExtensionConfiguration() {
+		return unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['naw_securedl']);
+	}
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -7,9 +7,21 @@ $TYPO3_CONF_VARS['FE']['eID_include']['tx_nawsecuredl'] = 'EXT:naw_securedl/clas
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = ':&Tx_NawSecuredl_Service_SecureDownloadService->parseFE';
 
 # TYPO3 > 6.0 (will be ignored in lower versions)
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Resource\\Driver\\LocalDriver'] = array(
-	'className' => 'Bitmotion\\NawSecuredl\\Resource\\Driver\\LocalDriver',
-);
+
+/*
+ * Only used in the BE Mode, the FE does a htmlspecialchars() somewhere //TODO: FIXME this must work in FE
+ */
+if (TYPO3_MODE == 'BE'){
+	if (class_exists('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')) {
+		\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
+			'TYPO3\\CMS\\Core\\Resource\\ResourceStorage',
+			\TYPO3\CMS\Core\Resource\ResourceStorage::SIGNAL_PreGeneratePublicUrl,
+			'Bitmotion\\NawSecuredl\\Resource\\UrlGenerationInterceptor',
+			'getPublicUrl'
+		);
+	}
+}
+
 
 # TYPO3 < 6.0 (will be ignored in higher versions)
 $TYPO3_CONF_VARS['BE']['XCLASS']['typo3/class.file_list.inc'] = t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Xclass/class.ux_fileList.inc';
