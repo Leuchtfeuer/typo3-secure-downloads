3ec5b4f720ca217291b7914711d7c1fbe7bd5b73
Florian Wessels <f.wessels@bitmotion.de>
2019-11-20 17:44:58 +0100

    [TASK] Update php docs for classes

--- Classes/Resource/Publishing/ResourcePublisher.php
+++ Classes/Resource/Publishing/ResourcePublisher.php
@@ -2,29 +2,17 @@
 declare(strict_types=1);
 namespace Bitmotion\SecureDownloads\Resource\Publishing;
 
-/***************************************************************
- *  Copyright notice
+/***
  *
- *  (c) 2013 Bitmotion GmbH (typo3-ext@bitmotion.de)
+ * This file is part of the "Secure Downloads" Extension for TYPO3 CMS.
  *
- *  All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is part of the Typo3 project. The Typo3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  (c) 2019 Florian Wessels <f.wessels@bitmotion.de>, Bitmotion GmbH
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ ***/
+
 use Bitmotion\SecureDownloads\Configuration\ConfigurationManager;
 use Bitmotion\SecureDownloads\Security\Authorization\Resource\Apache2AccessRestrictionPublisher;
 use TYPO3\CMS\Core\Resource\ResourceInterface;
