1f323bd5274157f21ee93ca6ed095fad3f1a06f8
Florian Wessels <flo.wessels@gmail.com>
2020-05-06 17:27:35 +0200

    [TASK] Add missing php docs

--- Classes/Resource/Event/AfterFileRetrievedEvent.php
+++ Classes/Resource/Event/AfterFileRetrievedEvent.php
@@ -2,6 +2,17 @@
 declare(strict_types = 1);
 namespace Leuchtfeuer\SecureDownloads\Resource\Event;
 
+/***
+ *
+ * This file is part of the "Secure Downloads" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2020 Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ *
+ ***/
+
 final class AfterFileRetrievedEvent
 {
     private $file;
