73cea3e77827c2336fb18b6de701c6c98adcd588
Jan-Michael Loew <c4ll-m3-j4ck@users.noreply.github.com>
2021-11-08 17:01:42 +0100

    FEATURE: Increase max logged filesize (#110)
    * FEATURE: Increase max logged filesize
    
    * BUGFIX: Prevent forced stream

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE tx_securedownloads_domain_model_log (
 	file_id varchar(255) DEFAULT NULL,
 	file_name varchar(255) DEFAULT '' NOT NULL,
 	file_path varchar(255) DEFAULT '' NOT NULL,
-	file_size int(11) DEFAULT '0' NOT NULL,
+	file_size bigint(20) DEFAULT 0 NOT NULL,
 	file_type varchar(255) DEFAULT '' NOT NULL,
 	media_type varchar(255) DEFAULT '' NOT NULL,
 	protected varchar(255) DEFAULT '' NOT NULL,
