449a779ee6c5bcfe09c8dc74bbd899f583bba103
Sebastian Afeldt <33094668+bmafeldt@users.noreply.github.com>
2023-05-26 08:33:10 +0200

    [TASK] Update authors and funding links. (#155)

--- Classes/Resource/Event/AfterFileRetrievedEvent.php
+++ Classes/Resource/Event/AfterFileRetrievedEvent.php
@@ -10,7 +10,7 @@ namespace Leuchtfeuer\SecureDownloads\Resource\Event;
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *
- *  (c) 2020 Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ *  (c) 2020 Dev <dev@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
  *
  ***/
 
