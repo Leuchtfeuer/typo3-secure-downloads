ff27a1a09fc1c5ac7c141435ab2c699b6d041aa4
Sebastian Afeldt <33094668+bmafeldt@users.noreply.github.com>
2023-06-27 16:20:36 +0200

    Use JavaScript date time picker to get consistent browser support. TER-108 (#167)
    * [TASK][WIP] Refactor code for PHP >=7.4.
    
    * [TASK][WIP] Refactor Traffic Log backend module and its templates.
    - Use be_user module data instead of PHP Session to store filter settings.
    - Remove 'showAction', as this was almost the same as 'listAction'. Therefore, traffic data per page can be viewed by select a page, all traffic can be viewed per click on TYPO3 root page UID 0.
    - Removed/replaced deprecated functions.
    - Icon factory added.
    - Module registration moved.
    
    * [TASK][WIP] Remove extension configuration 'Log Module (logging.log [boolean])'.
    - The module will now always be active.
    
    * [TASK] Undo removal of extension configuration 'log'. TER-108 TER-116
    
    * [TASK] Update testing framework. TER-108 TER-119
    
    * [BUGFIX] Test 'whenALinkWithAnOtherUserIDofTheCurrentUserLinkResponseBodyIsModified' fails. TER-108 TER-119
    This is exactly the '[BUGFIX] JWT::decode method signature not respected #163' - Thanks to https://github.com/jpmschuler
    
    * [TASK] Move typecast to variable assignment. TER-108 TER-116
    
    * [TASK] Update extension documentation. TER-108 TER-111
    
    * [TASK] Use JS date-time-picker from cms-belog. TER-108 TER-122
    
    * [TASK] Simplify code. TER-108 TER-122

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dependencies' => [
+        'backend',
+        'core',
+    ],
+    'imports' => [
+        '@leuchtfeuer/secure-downloads/' => 'EXT:secure_downloads/Resources/Public/JavaScript/',
+    ],
+];
