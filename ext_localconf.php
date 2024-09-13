adeb4d94ec9bb6cc8931fa604402b23f8e490a95
Oliver Heins <o.heins@leuchtfeuer.com>
2024-09-13 17:20:16 +0200

    [BUGFIX] Do not override internal TYPO3 list [TER-236] (#205)
    Try to get MimeType via TYPO3 buildin logic first. If that fails, use our
    extended file extension list. Do not override TYPO3's internal
    fileExtensionToMimeType list!

--- ext_localconf.php
+++ ext_localconf.php
@@ -48,9 +48,6 @@ call_user_func(
             true
         );
 
-        // Add MimeTypes
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType'] += \Leuchtfeuer\SecureDownloads\MimeTypes::ADDITIONAL_MIME_TYPES;
-
         // Schedulter task
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables']['tx_securedownloads_domain_model_log'] = [
             'dateField' => 'tstamp',
