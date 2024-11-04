8d10e6dd790e29ba461673a9cdea492f2b264011
Peter Zar <p.zar@leuchtfeuer.com>
2024-11-04 09:08:24 +0100

    [TASK] remove no group by SQL mode from default FunctionalTestCase config

--- Tests/Functional/Domain/Repository/LogRepositoryTest.php
+++ Tests/Functional/Domain/Repository/LogRepositoryTest.php
@@ -12,6 +12,15 @@ class LogRepositoryTest extends FunctionalTestCase
     protected array $testExtensionsToLoad = ['leuchtfeuer/secure-downloads'];
     private LogRepository  $logRepository;
 
+    protected array $configurationToUseInTestInstance = [
+       'DB' =>
+           ['Connections' =>
+               ['Default' =>
+                   ['initCommands' => 'SET SESSION sql_mode = \'STRICT_ALL_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_VALUE_ON_ZERO,NO_ENGINE_SUBSTITUTION,NO_ZERO_DATE,NO_ZERO_IN_DATE\';']
+               ]
+           ]
+    ];
+
     public function setUp(): void
     {
         parent::setUp();
