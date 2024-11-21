696753721c794dcc67b65f159c7ab3d270a26328
Marcus Balasch <m.balasch@leuchtfeuer.com>
2024-11-21 17:14:31 +0100

    [BUGFIX] Resource files are now also logged #220 [TER-262]

--- Classes/Resource/FileDelivery.php
+++ Classes/Resource/FileDelivery.php
@@ -89,6 +89,16 @@ class FileDelivery implements SingletonInterface
 
         if (file_exists($file)) {
             $fileObject = $this->resourceFactory->retrieveFileOrFolderObject($this->token->getFile());
+
+            if ($this->extensionConfiguration->isLog()) {
+                $this->token->log([
+                    'fileSize' => $fileSize = (int)filesize($file),
+                    'mimeType' => (new FileInfo($file))->getMimeType()
+                        ?: $this->guessMimeTypeByFileExtension($file)
+                            ?: MimeTypes::DEFAULT_MIME_TYPE,
+                ]);
+            }
+
             if ($fileObject instanceof File) {
                 $response = $fileObject
                     ->getStorage()
@@ -221,13 +231,6 @@ class FileDelivery implements SingletonInterface
 
         $this->dispatchBeforeFileDeliverEvent($outputFunction, $header, $fileName, $mimeType, $forceDownload);
         $this->header = $header;
-
-        if ($this->extensionConfiguration->isLog()) {
-            $this->token->log([
-                'fileSize' => $fileSize,
-                'mimeType' => $mimeType,
-            ]);
-        }
 
         return $this->outputFile($outputFunction, $file) ?? 'php://temp';
     }
