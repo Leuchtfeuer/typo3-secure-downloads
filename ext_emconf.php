0f2c86f1e18c27e3aeb65180dc6531bf0195a866
Niklas Grieger <n.grieger@leuchtfeuer.com>
2024-12-10 12:44:29 +0100

    [TASK] Adjust PHP constraints [TER-249] [TER-250]

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF['secure_downloads'] = [
     'author_company' => 'Leuchtfeuer Digital Marketing',
     'constraints' => [
         'depends' => [
-            'php' => '8.3.0-8.3.99',
+            'php' => '8.2.0-8.4.99',
             'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [
