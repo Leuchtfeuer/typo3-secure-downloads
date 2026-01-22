36fae9adb3761b00c525ff0acd145db19243044d
Niklas Grieger <n.grieger@leuchtfeuer.com>
2026-01-22 12:38:40 +0100

    [TASK] Cleanup code [TER-373] [TER-375]

--- ext_tables.php
+++ ext_tables.php
@@ -1,24 +1,27 @@
 <?php
 
-use Psr\EventDispatcher\EventDispatcherInterface;
-use Psr\Log\LoggerInterface;
+use Leuchtfeuer\SecureDownloads\Domain\Repository\StorageRepository;
+use Leuchtfeuer\SecureDownloads\Domain\Transfer\ExtensionConfiguration;
+use Psr\Log\NullLogger;
 use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\CMS\Core\Resource\Driver\DriverRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(
     function ($extensionKey) {
         // Create resource storage
-        if ((new \Leuchtfeuer\SecureDownloads\Domain\Transfer\ExtensionConfiguration())->isCreateFileStorage()) {
-            $storageRepositoryClass = \Leuchtfeuer\SecureDownloads\Domain\Repository\StorageRepository::class;
-            $eventDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\EventDispatcher\EventDispatcher::class);
-            $connectionPool = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class);
-            $driverRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class);
-            $flexFormTools = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class);
-            $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Psr\Log\NullLogger::class);
-            $storageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance($storageRepositoryClass, $eventDispatcher, $connectionPool, $driverRegistry, $flexFormTools, $logger);
+        if ((new ExtensionConfiguration())->isCreateFileStorage()) {
+            $storageRepositoryClass = StorageRepository::class;
+            $eventDispatcher = GeneralUtility::makeInstance(EventDispatcher::class);
+            $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+            $driverRegistry = GeneralUtility::makeInstance(DriverRegistry::class);
+            $flexFormTools = GeneralUtility::makeInstance(FlexFormTools::class);
+            $logger = GeneralUtility::makeInstance(NullLogger::class);
+            $storageRepository = GeneralUtility::makeInstance($storageRepositoryClass, $eventDispatcher, $connectionPool, $driverRegistry, $flexFormTools, $logger);
             $storageRepository->createSecureDownloadStorage();
         }
     }, 'secure_downloads'
